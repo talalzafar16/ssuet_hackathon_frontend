ba9fbb36b9d058d3a58b3beba10ae4febdf44acd
Ismail0709 <ismailbajwa2003@gmail.com>
2025-01-23 03:01:37 +0500

    profile page

--- src/compoenents/router/index.tsx
+++ src/compoenents/router/index.tsx
@@ -3,6 +3,7 @@ import Home from '../../pages/home/page';
 import Donate from '../../pages/donate/page';
 import LoginSignup from '../../pages/auth/login/page';
 import NGOlogin from '../../pages/auth/NGOlogin/page';
+import ProfilePage from '../../pages/profile/page';
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/donate" element={<Donate />} />
         <Route path="/auth/login" element={<LoginSignup />} />
         <Route path="/auth/NGOlogin" element={<NGOlogin />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   )
